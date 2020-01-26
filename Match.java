f452eca9bcadc76d85c8ea70e7da8f95e8dcbb6e
Vitalii Bondalietov <bondaletov.vitaly01@gmail.com>
2020-01-26 11:05:47 +0200

    Added Match.java

---
+++ Match.java
@@ -0,0 +1,56 @@
+
+public class Match {
+
+	private String season;
+	private String nameOfGame;
+	private int goalScored;
+	private int goalConceded;
+		
+	public Match(String season, String nameOfGame, int goalScored, int goalConceded) {
+	    this.season = season;
+	    this.nameOfGame = nameOfGame;
+	    this.goalScored = goalScored;
+	    this.goalConceded = goalConceded;
+	}
+	
+	public String getSeason(){
+		return season;
+	}
+	
+	public void setSeason(String season){
+		this.season = season;
+	}
+	
+	public String getNameOfGame(){
+		return nameOfGame;
+	}
+	
+	public void setNameOfGame(String nameOfGame){
+		this.nameOfGame = nameOfGame;
+	}
+	
+	public int getGoalScored(){
+		return goalScored;
+	}
+	
+	public void setGoalScored(int goalScored){
+		this.goalScored = goalScored;
+	}
+	
+	public int getGoalConceded(){
+		return goalConceded;
+	}
+	
+	public void setGoalConceded(int goalConceded){
+		this.goalConceded = goalConceded;
+	}
+	
+	public void infoAboutOneGame(){
+		System.out.println(season + ", " + nameOfGame + " " + goalScored + ":" + goalConceded);
+	}
+	
+	public static void main (String [] args){
+		Match one = new Match("2019/20", "Shakhtar Donetsk-Manchester City", 0, 3);
+		one.infoAboutOneGame();
+	}
+}
